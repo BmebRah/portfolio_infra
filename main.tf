aeb36712971861047de3442c5acc9a5aeea0441c
mebrah <mebrahtubereket@gmail.com>
2023-12-07 18:43:14 +0000

    Provision a web server on digitalocean

--- main.tf
+++ main.tf
@@ -7,21 +7,29 @@ terraform {
     }
 }
 
+variable "digitalocean_token" {
+    
+}
+
 provider "digitalocean" {
     token = "${var.digitalocean_token}"
 }
 
 resource "digitalocean_ssh_key" "default" {
-    name = "web_server_token"
+    name = "ssh key"
     public_key = "${file("~/.ssh/id_rsa.pub")}"
 
 }
 
-resource "digitalocean_droplet" "web" {
+resource "digitalocean_droplet" "node1" {
     name  =  "web1"
     image =  "ubuntu-22-04-x64"
     size  =  "s-1vcpu-1gb"
     region = "lon1"
-    ssh_keys =["$digitalocean_ssh_key.defaults.fingerprint}"]
+    ssh_keys =[digitalocean_ssh_key.default.fingerprint]
 
+}
+
+output "ipv4_address" {
+    value = digitalocean_droplet.node1.ipv4_address
 }
