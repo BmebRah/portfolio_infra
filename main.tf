e3611d46913aab9623a2e7b2e28ce2e4e84637b0
mebrah <mebrahtubereket@gmail.com>
2023-12-08 16:06:16 +0000

    Fixs formatting

--- main.tf
+++ main.tf
@@ -1,35 +1,35 @@
 terraform {
-    required_providers {
-        digitalocean = {
-            source  = "digitalocean/digitalocean"
-            version = "~> 2.0"
-        }
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
     }
+  }
 }
 
 variable "digitalocean_token" {
-    
+
 }
 
 provider "digitalocean" {
-    token = "${var.digitalocean_token}"
+  token = var.digitalocean_token
 }
 
 resource "digitalocean_ssh_key" "default" {
-    name = "ssh key"
-    public_key = "${file("~/.ssh/id_rsa.pub")}"
+  name       = "ssh key"
+  public_key = file("~/.ssh/id_rsa.pub")
 
 }
 
 resource "digitalocean_droplet" "node1" {
-    name  =  "web1"
-    image =  "ubuntu-22-04-x64"
-    size  =  "s-1vcpu-1gb"
-    region = "lon1"
-    ssh_keys =[digitalocean_ssh_key.default.fingerprint]
+  name     = "web1"
+  image    = "ubuntu-22-04-x64"
+  size     = "s-1vcpu-1gb"
+  region   = "lon1"
+  ssh_keys = [digitalocean_ssh_key.default.fingerprint]
 
 }
 
 output "ipv4_address" {
-    value = digitalocean_droplet.node1.ipv4_address
+  value = digitalocean_droplet.node1.ipv4_address
 }
