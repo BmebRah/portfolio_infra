1be89e2de5019ee172cd3282c000e3705e82d5bc
Bereket <mebrahtubereket@gmail.com>
2024-01-26 23:32:33 +0000

    Formats terraform files

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~>5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "eu-west-2"
+  region = "eu-west-2"
 }
 
 resource "aws_ecr_repository" "backend" {
-    name                 = "portfolio-aws-backend"
-    image_tag_mutability = "MUTABLE"
+  name                 = "portfolio-aws-backend"
+  image_tag_mutability = "MUTABLE"
 
 }
 
