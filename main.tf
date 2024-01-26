afb8f70919f92f640f850b6c61c92577f347218d
Bereket <mebrahtubereket@gmail.com>
2024-01-26 23:16:59 +0000

    Updates main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,20 @@
 terraform {
-required_providers {
-    aws = {
-    source  = "hashicorp/aws"
-    version = "~> 5.0"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~>5.0"
+        }
     }
 }
-}
-
-
 
 provider "aws" {
-region = "eu-west-2"
+    region = "eu-west-2"
+}
+
+resource "aws_ecr_repository" "backend" {
+    name                 = "portfolio-aws-backend"
+    image_tag_mutability = "MUTABLE"
+
 }
 
 
-resource "aws_instance" "bereket-ec2" {
-instance_type = "t2.micro"
-key_name      = "proxy_key"
-ami           = "ubuntu"
-
-
-}
-
-output "ipv4_address" {
-value = aws_instance.bereket-ec2.key_name
-}
